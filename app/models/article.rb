d459c10d7a1c3510182cb05df1e1eb81dacd2dbd
savka-olia <tokarchykolia@gmail.com>
2022-05-26 15:29:50 +0300

    add associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, presence: true, length: { minimum: 2, maximum: 50 }
   validates :description, presence: true, length: { in: 10..100, wrong_length:"Wrong number of characters!" }
 end
