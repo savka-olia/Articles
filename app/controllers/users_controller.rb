e6d5bf7f97d2cd761960cce733b36706baeae064
savka-olia <tokarchykolia@gmail.com>
2022-05-30 21:42:38 +0300

    Authentication helper methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,45 +1,47 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+  def show
+    @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  end
 
-    def show
-      @user = User.find(params[:id])
-      @articles = @user.articles.paginate(page: params[:page], per_page: 5)
+  def index
+    @users = User.paginate(page: params[:page], per_page: 5)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "Your account information successfully updated"
+      redirect_to @user
+    else
+      render 'edit'
     end
+  end
 
-    def index
-      @users = User.paginate(page: params[:page], per_page: 5)
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = "Welcome to Alpha Blog #{ @user.username }, you have successfully signed up!"
+      redirect_to articles_path
+    else
+      render 'new'
     end
+  end
 
-    def new
-      @user = User.new
-    end
+  private
 
-    def edit
-      @user = User.find(params[:id])
-    end
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 
-    def update
-      @user = User.find(params[:id])
-      if @user.update(user_params)
-        flash[:notice] = "Your account information successfully updated"
-        redirect_to @user
-      else
-        render 'edit'
-      end
-    end
-
-    def create
-      @user = User.new(user_params)
-      if @user.save
-        flash[:notice] = "Welcome to Alpha Blog #{ @user.username }, you have successfully signed up!"
-        redirect_to articles_path
-      else
-        render 'new'
-      end
-    end
-
-    private
-
-    def user_params
-      params.require(:user).permit(:username, :email, :password)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
